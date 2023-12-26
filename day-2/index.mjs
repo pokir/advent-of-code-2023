6c0ae9c5bf4c6d787d6b3c86b5ff1dbbc48f35bb
pokir <pokir@users.noreply.github.com>
2023-12-26 16:31:23 +0100

    Finish day 2

--- day-2/index.mjs
+++ day-2/index.mjs
@@ -1,38 +1,63 @@
 import { readFileSync } from 'fs';
 
 
-const input = process.env.AOC_INPUT;
+const games = process.env.AOC_INPUT
+  .split('\n')
+  .map(line => line.split(':')[1])
+  .map(line => line.split(';'))
+  .map(line => line.map(hand => hand.split(',')))
+  .map(line => line.map(hand => hand.map(string => {
+    const [ _, number, color ] = string.split(' ');
+    return [Number.parseInt(number), color];
+  })))
+
+
 
 
 function partOne() {
   let sum = 0;
 
-  input
-    .split('\n')
-    .map(line => line.split(':')[1])
-    .slice(0, -1)
-    .map(line => line.split(';'))
-    .map(line => line.map(hands => hands.split(',')))
-    .forEach((line, index) => {
-      let good = true;
+  games.forEach((game, index) => {
+    let good = true;
 
-      for (const hands of line) {
-        for (const hand of hands) {
-          const [ _,  number, color ] = hand.split(' ');
-          if ((color === 'red' && number > 12)
-            || (color === 'green' && number > 13)
-            || (color === 'blue' && number > 14))
-            good = false;
-        }
-      }
-
-      if (good) sum += index + 1;
+    game.forEach(hand => {
+      hand.forEach(([ number, color ]) => {
+        if ((color === 'red' && number > 12)
+          || (color === 'green' && number > 13)
+          || (color === 'blue' && number > 14))
+          good = false;
+      });
     });
 
-  console.log(sum);
+    if (good) sum += index + 1;
+  });
+
+  console.log('Part one:', sum);
+}
+
+
+function partTwo() {
+  let sumOfPowers = 0;
+
+  games.forEach(game => {
+    const maxes = {red: 0, blue: 0, green: 0};
+
+    game.forEach(hand => {
+      hand.forEach(([ number, color ]) => {
+        if (number > maxes[color])
+          maxes[color] = number;
+      });
+    });
+
+    console.log(game);
+    console.log(maxes);
+
+    sumOfPowers += maxes.red * maxes.blue * maxes.green;
+  });
+
+  console.log('Part two:', sumOfPowers);
 }
 
 
 partOne();
-
-
+partTwo();
