aaec8b7a75f8c72dc08d8978ed4e3178f0dfe753
pokir <pokir@users.noreply.github.com>
2023-12-28 17:20:18 +0100

    Finish day 4 part two

--- day-4/Main.java
+++ day-4/Main.java
@@ -10,26 +10,39 @@ public class Main {
 
     int totalPoints = 0;
 
-    for (String line : lines) {
+    int[] cardInstances = new int[lines.length];
+    Arrays.fill(cardInstances, 1);
+
+    for (int i = 0; i < lines.length; ++i) {
+      String line = lines[i];
+
       String[] numbers = line.split(":")[1].split("\\|");
 
       List<String> winningNumbers = new LinkedList<>(Arrays.asList(numbers[0].trim().split(" +")));
       String[] yourNumbers = numbers[1].trim().split(" +");
 
       int points = 0;
+      int numMatches = 0;
 
       for (String number : yourNumbers) {
         if (winningNumbers.contains(number)) {
           if (points == 0) points = 1;
           else points *= 2;
 
+          ++numMatches;
+
           winningNumbers.remove(number);
         }
       }
 
       totalPoints += points;
+
+      for (int j = i + 1; j < Math.min(i + numMatches + 1, lines.length); ++j) {
+        cardInstances[j] += cardInstances[i];
+      }
     }
 
     System.out.println("Part one: " + totalPoints);
+    System.out.println("Part two: " + Arrays.stream(cardInstances).sum());
   }
 }
